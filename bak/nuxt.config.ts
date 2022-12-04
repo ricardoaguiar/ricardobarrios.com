6b84853b0566e8efcf68fba43840ad91597f0b04
ricardoaguiar <barrios.dev@gmail.com>
2022-12-04 23:14:26 +0100

    updated app

---
+++ bak/nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
+export default defineNuxtConfig({
+
+    components: false,
+    ssr: false,
+
+    modules: [
+        '@nuxt/content'
+    ],
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/assets/_colors.scss" as *;'
+                }
+            }
+        }
+    }
+
+})
