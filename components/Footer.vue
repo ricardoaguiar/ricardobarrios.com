8a187b9f0584c8f86f31f3532d6f32feac5ff2e9
ricardoaguiar <barrios.dev@gmail.com>
2022-12-09 21:23:13 +0100

    updated directories

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="footer">
+   footer
+ </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+.footer{
+  width: 100vw;
+  height: 10rem;
+  bottom: 0;
+  margin: 0 auto;
+  background: rgba(0, 0, 0, 0.98);
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,24 @@
 <template>
+  <div class="container">
   <Header />
     <slot />
+  <Footer />
+  </div>
 </template>
 
 <script>
+import Footer from "~/components/Footer.vue";
+
 export default {
-  name: "default.vue"
+  name: "default.vue",
+  components: {Header,Footer}
 }
 </script>
 
 <style scoped>
+.container{
+  margin: 0;
+  background-color: #dedede
+}
 
 </style>
