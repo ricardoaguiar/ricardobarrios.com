10cae9b38338e4f16b646c98c78b42f712d6994d
ricardoaguiar <barrios.dev@gmail.com>
2022-12-27 23:03:57 +0100

    added blog page

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+  <div class="p-6 border bg-white dark:bg-black dark:border-gray-700 rounded">
+    <ContentDoc />
+  </div>
+  <Footer />
+</template>
+
+<script>
+export default {
+  name: "[...slug].vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,14 +2,13 @@
 import { convertDate } from '~/utils';
 
 const contentQuery = await queryContent('blog').sort({ author: 0 }).find();
-console.log(contentQuery);
 </script>
 
 <template>
 	<Header />
-	<main class="bg-white px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pb-28 lg:pb-28 lg:pt-24">
+	<main class="bg-white px-4 pt-10 pb-20 sm:px-6 lg:px-8 lg:pb-28 lg:pb-28 lg:pt-12">
 		<div class="mx-w-lg lg:mx-w-7xl mx-auto">
-			<div class="mb-10 border-b border-gray-200 pb-10">
+			<div class="mb-8 border-b border-gray-200 pb-4">
 				<h2 class="text-4xl font-semibold tracking-tight text-gray-900 sm:text-4xl">Blog</h2>
 			</div>
 			<div
