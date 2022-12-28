50a9be2de7c0aab6c70b5397d1e9191ea030c7af
ricardoaguiar <barrios.dev@gmail.com>
2022-12-28 22:46:17 +0100

    added slug style

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,6 +1,6 @@
 <template>
   <Header />
-  <div class="p-6 border bg-white dark:bg-black dark:border-gray-700 rounded">
+  <div class="container bg-white mx-auto m-16 p-10">
     <ContentDoc />
   </div>
   <Footer />
