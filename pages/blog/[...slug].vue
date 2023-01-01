920ff5a6612c06c493e30dbeffcc40892d078c7e
ricardoaguiar <barrios.dev@gmail.com>
2023-01-01 21:28:47 +0100

    added cloudinary

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,7 +2,7 @@
   <Header />
   <div class="container bg-white mx-auto m-16 p-10">
     <ContentDoc />
-    <NuxtImg provider="cloudinary" src="https://res.cloudinary.com/ricardoaguiar/image/upload/v1653078865/20100202.Caribean.Cruise.0624_vzhafb.jpg" width="300" height="200" />
+    <NuxtImg provider="cloudinary" src="https://res.cloudinary.com/ricardoaguiar/image/upload/v1625499913/blog/therock.jpg" width="300" height="200" />
 
   </div>
   <Footer />
