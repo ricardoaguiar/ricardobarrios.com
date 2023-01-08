185588b1877760411454fa19dee3790054cca011
ricardoaguiar <barrios.dev@gmail.com>
2023-01-08 16:29:54 +0100

    update method

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,17 +1,43 @@
-<script setup>
+<script setup lang='ts'>
 import { convertDate } from '~/utils';
 
-const contentQuery = await queryContent('blog').sort({ author: 0 }).find();
+const formatter = Intl.DateTimeFormat(undefined, {
+	year: 'numeric',
+	month: 'short',
+	day: 'numeric'
+});
+
+// const contentQuery = await queryContent('blog').only('_path', 'title').sort({ author: 0 }).find();
+const { data: contentQuery } = useAsyncData(
+	() => queryContent('blog').only(['_path', 'title', 'date']).find(),
+	{
+		transform: (data) =>
+			data.map((x) => ({
+				...x,
+				textDate: formatter.format(new Date(x.date))
+			}))
+	}
+);
 </script>
 
 <template>
-	<main class="bg-white px-4 pt-10 pb-20 sm:px-6 lg:px-8 lg:pb-28 lg:pb-28 lg:pt-12">
+	<main class="bg-white px-4 pt-10 pb-20 sm:px-6 lg:px-8 lg:pb-28 lg:pt-12">
+		<!-- <pre class="text-xs">
+ 		{{ contentQuery }}
+		</pre -->
+		>
 		<div class="mx-w-lg lg:mx-w-7xl mx-auto">
 			<div class="mb-8 border-b border-gray-200 pb-4">
 				<h2 class="text-4xl font-semibold tracking-tight text-gray-900 sm:text-4xl">Blog</h2>
 			</div>
 			<div
-				class="mt-12 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2 sm:gap-x-8 lg:grid-cols-3 lg:gap-x-6"
+				class="
+					mt-12
+					grid grid-cols-1
+					gap-y-12 gap-x-6
+					sm:grid-cols-2 sm:gap-x-8
+					lg:grid-cols-3 lg:gap-x-6
+				"
 			>
 				<div
 					v-for="article in contentQuery"
@@ -36,13 +62,7 @@ const contentQuery = await queryContent('blog').sort({ author: 0 }).find();
 			</div>
 		</div>
 	</main>
-
 </template>
 
-<script>
-export default {
-	name: 'index'
-};
-</script>
 
 <style scoped></style>
